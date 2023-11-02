2fed97ec8175b89fb3f22607f633681f116181c5
patrice ELIE DIT COSAQUE <patriceedc@hotmail.com>
2023-11-02 15:04:57 +0100

    tp2

--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -4,16 +4,17 @@ import entites2.Personne;
 
 public class TestPersonne {
     public static void main(String[] args) {
-        AdressePostale adr3 = new AdressePostale();
-        adr3.numeroRue = 1;
-        adr3.libelleRue = "Allée Michel Piquemal";
-        adr3.codePostal = 34830;
-        adr3.ville = "Jacou";
+        AdressePostale adr3 = new AdressePostale( 1, "Allée Michel piquemal", 34830, "Jacou");
+       // adr3.numeroRue = 1;
+       // adr3.libelleRue = "Allée Michel Piquemal";
+       // adr3.codePostal = 34830;
+        //adr3.ville = "Jacou";
 
-        Personne pe1 = new Personne();
-        pe1.nom = "ELIE DIT COSAQUE";
-        pe1.prenom = "Patrice";
-        pe1.adresse = adr3;
+        Personne pe1 = new Personne("ELIE DIT COSAQUE", "Patrice", adr3);
+        //pe1.nom = "ELIE DIT COSAQUE";
+       // pe1.prenom = "Patrice";
+        //pe1.adresse = adr3;
 
+        Personne pe2 = new Personne("MICHEL", "Patrick", adr3);
     }
 }
--- src/entites2/Personne.java
+++ src/entites2/Personne.java
@@ -6,5 +6,16 @@ public class Personne {
     public String nom;
     public String prenom;
     public AdressePostale adresse;
+    public Personne(String nom, String prenom, AdressePostale adresse)
+    {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+    }
+    public Personne(String nom, String prenom){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = null;
+    }
 
 }
