07bae7a166b1da1fd222b331132c06e429a79c39
patrice ELIE DIT COSAQUE <patriceedc@hotmail.com>
2023-11-02 15:20:50 +0100

    mise aj

--- src/entites/TestAdressePostale.java
+++ src/entites/TestAdressePostale.java
@@ -1,17 +1,11 @@
 package entites;
 
 public class TestAdressePostale {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         AdressePostale adr1 = new AdressePostale(1, "Allée Michel Piquemal", 34830,"Jacou");
-        //adr1.numeroRue = 1;
-       // adr1.libelleRue = "Allée Michel Piquemal";
-       // adr1.codePostal = 34830;
-        //adr1.ville = "Jacou";
 
         AdressePostale adr2 = new AdressePostale(2, "RUE MICHEL", 34000, "Montpellier");
-       // adr2.numeroRue = 2;
-       // adr2.libelleRue = "RUE MICHEL";
-        //adr2.codePostal = 34000;
-        //adr2.ville = "Montpellier";
+
     }
 }
--- src/entites2/Personne.java
+++ src/entites2/Personne.java
@@ -15,7 +15,6 @@ public class Personne {
     public Personne(String nom, String prenom){
         this.nom = nom;
         this.prenom = prenom;
-        this.adresse = null;
     }
 
 }
